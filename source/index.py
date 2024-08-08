f96e1014ce9ceb6ba2c6d10912b06ac254f8ddf3
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-08-08 14:38:33 +0300

    Change index_store to vector_store and format

--- source/index.py
+++ source/index.py
@@ -3,16 +3,21 @@ import llama_index.embeddings.huggingface as huggingface
 
 
 def main():
-    llama_index.core.Settings.embed_model = huggingface.HuggingFaceEmbedding(
-        model_name="BAAI/bge-small-en-v1.5"
+    llama_index.core.Settings.embed_model = (
+        huggingface.HuggingFaceEmbedding(
+            model_name="BAAI/bge-small-en-v1.5"
+        )
     )
-
-    documents = llama_index.core.SimpleDirectoryReader("./data").load_data()
+    documents = llama_index.core.SimpleDirectoryReader(
+        "./data"
+    ).load_data()
     index = llama_index.core.VectorStoreIndex.from_documents(
         documents, show_progress=True
     )
-    storage_context = llama_index.core.storage.StorageContext.from_defaults(
-        index_store=index
+    storage_context = (
+        llama_index.core.storage.StorageContext.from_defaults(
+            vector_store=index
+        )
     )
     storage_context.persist()
 
