6b5c65c3537bbad077870c843c0eb389e74fe2c7
Ilyas Mustafazade <il.mystafa@gmail.com>
2024-08-09 00:34:40 +0300

    Add query engine and search functions

--- source/search.py
+++ source/search.py
@@ -1,7 +1,20 @@
 import llama_index
 from llama_index.core.storage import StorageContext
-import llama_index.embeddings.huggingface as huggingface
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.huggingface import HuggingFaceLLM
 import termcolor
+
+
+def do_semantic_search(query_engine, query_string):
+    return query_engine.query(query_string)
+
+
+def do_keyword_search(query_string):
+    pass
+
+
+def do_hybrid_search(query_string):
+    pass
 
 
 def main():
@@ -9,13 +22,31 @@ def main():
     storage_context = StorageContext.from_defaults(
         persist_dir="storage"
     )
-
     index = llama_index.core.load_index_from_storage(
         storage_context,
-        embed_model=huggingface.HuggingFaceEmbedding(
+        embed_model=HuggingFaceEmbedding(
             model_name="BAAI/bge-small-en-v1.5"
         ),
     )
+
+    termcolor.cprint("Performing searches", "blue")
+    query_engine = index.as_query_engine(
+        llm=HuggingFaceLLM(
+            model_name="HuggingFaceH4/zephyr-7b-beta",
+            tokenizer_name="HuggingFaceH4/zephyr-7b-beta",
+            context_window=3900,
+            max_new_tokens=256,
+            model_kwargs={},
+            generate_kwargs={
+                "temperature": 0.7,
+                "top_k": 50,
+                "top_p": 0.95,
+            },
+            device_map="auto",
+        )
+    )
+
+    print(do_semantic_search(query_engine, "Deadpool"))
 
 
 if __name__ == "__main__":
